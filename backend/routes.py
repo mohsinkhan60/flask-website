ce015863af31c73c5400b2fd40126107af864b59
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-01-29 18:59:54 +0500

    Add API route to create a new friend with validation and image URL generation

--- backend/routes.py
+++ backend/routes.py
@@ -8,3 +8,37 @@ def get_friends():
     friends = Friend.query.all()
     result = [friend.to_json() for friend in friends]
     return jsonify(result)
+
+#create a new friend
+@app.route('/api/friends', methods=['POST'])
+def create_friend():
+    try:
+        data = request.json
+
+        required_fields = ["name", "role","gender", "description"]
+        for field in required_fields:
+            if field not in data:
+                return jsonify({'msg': f'{field} is required'}), 400
+        
+        name = data.get('name')
+        role = data.get('role')
+        description = data.get('description')
+        gender = data.get('gender')
+
+
+        #For Gender
+        if gender == 'male':
+            img_url = f"https://avatar.iran.liara.run/public/boy?username={name}"
+        elif gender == 'female':
+            img_url = f"https://avatar.iran.liara.run/public/girl?username={name}"
+        else:
+            img_url = None
+
+
+        new_friend = Friend(name=name, role=role, description=description, gender=gender, img_url=img_url)
+        db.session.add(new_friend)
+        db.session.commit()
+        return jsonify({'msg':"Friends created successfully"}), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'msg':str(e)}), 500
