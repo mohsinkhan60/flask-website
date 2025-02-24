10f991b89c5b8f9fcab503f4b92e62229fa4c406
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-02-25 01:35:14 +0500

    Add frontend environment configuration file for development mode

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +9,5 @@ export default defineConfig({
     port: 3000,
   }
   
-})
+});
 
